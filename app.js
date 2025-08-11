8f94c357b8fa3a059090967e3ba1f49c0bedefcc
corazon469 <ramirez.samuelmeneses469@gmail.com>
2025-08-11 13:34:00 -0500

    javascript

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// losnomres
+let amigos = [];
+
+// agregar nombres
+function agregarAmigo() {
+    // entrada
+    let input = document.getElementById("nombreAmigo");
+    let nombre = input.value.trim();
+
+    // Validar que no esté vacío
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    // Agregar al array
+    amigos.push(nombre);
+
+    // Limpiar campo de entrada
+    input.value = "";
+
+    // Actualizar lista
+    mostrarLista();
+}
+
+// mostrar lsita
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar lista
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+// sortoear
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML =
+        "El amigo secreto es: <strong>" + amigoSorteado + "</strong>";
+}
